d05d1842fc3e24f292fcd28e6438e8657cbfabce
DiegoDom <dominguezdiex@gmail.com>
2022-05-13 17:25:38 -0500

    OpenJira init MaterialUI & UIContext

---
+++ themes/light-theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: grey[300],
+    },
+    primary: {
+      main: "#4a148c",
+    },
+    secondary: {
+      main: "#19857b"
+    },
+    error: {
+      main: "#f86c6b"
+    }
+  },
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        elevation: 0
+      }
+    },
+  }
+});
